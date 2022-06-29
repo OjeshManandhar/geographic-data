72e03da6822b2111770a42345cf2cd38947a7ecb
OjeshManandhar <ozes.manandhar@gmail.com>
2022-06-29 19:36:27 +0545

    Move `source` and `created` inside `src`

--- src/utils.ts
+++ src/utils.ts
@@ -5,7 +5,7 @@ const SOURCE_DIR = 'sources',
   DESTINATION_DIR = 'created';
 
 export function readJSONFile<T extends object>(fileName: string): T {
-  const filePath = path.join(__dirname, '..', SOURCE_DIR, fileName + '.json');
+  const filePath = path.join(__dirname, SOURCE_DIR, fileName + '.json');
 
   const content = require(filePath);
 
@@ -13,12 +13,7 @@ export function readJSONFile<T extends object>(fileName: string): T {
 }
 
 export function writeJSONFile(content: Array<object>, fileName: string) {
-  const filePath = path.join(
-    __dirname,
-    '..',
-    DESTINATION_DIR,
-    fileName + '.json',
-  );
+  const filePath = path.join(__dirname, DESTINATION_DIR, fileName + '.json');
 
   if (fs.existsSync(filePath)) {
     fs.rmSync(filePath);
