6fada13ff7ee82324614f1d48cb84bb3e7f8f6a7
OjeshManandhar <ozes.manandhar@gmail.com>
2022-06-29 14:37:15 +0545

    Install nodemon

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 const world = 'world';
 
+import { testStr } from './data/test';
+
 export function hello(who: string = world): string {
   return `Hello ${who}! `;
 }
 
-console.log('hello()', hello());
+console.log('hello():', hello());
+console.log('testStr():', hello(testStr));
