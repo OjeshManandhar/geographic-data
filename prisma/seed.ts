0ec10b8d320a6f23c6d73321842020d76a61bfe2
OjeshManandhar <ozes.manandhar@gmail.com>
2022-06-30 16:27:40 +0545

    Write seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,2 +1,40 @@
+// packages
 import { PrismaClient } from '@prisma/client';
+
+// data
+const flags = require('./../src/seedable/flags');
+const states = require('./../src/seedable/states');
+const countries = require('./../src/seedable/countries');
+const continents = require('./../src/seedable/continents');
+const currencies = require('./../src/seedable/currencies');
+
 const prisma = new PrismaClient();
+
+async function seed() {
+  await prisma.continent.createMany({
+    skipDuplicates: true,
+    data: continents,
+  });
+
+  await prisma.currency.createMany({
+    skipDuplicates: true,
+    data: currencies,
+  });
+
+  await prisma.country.createMany({
+    skipDuplicates: true,
+    data: countries,
+  });
+
+  await prisma.flag.createMany({
+    skipDuplicates: true,
+    data: flags,
+  });
+
+  await prisma.state.createMany({
+    skipDuplicates: true,
+    data: states,
+  });
+}
+
+seed();
