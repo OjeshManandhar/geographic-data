da7ef495e44a8db861bc01e6353618a3ce0d7cf1
OjeshManandhar <ozes.manandhar@gmail.com>
2022-06-30 15:33:19 +0545

    Crate prisma models

---
+++ prisma/migrations/20220630095858_init/migration.sql
@@ -0,0 +1,86 @@
+-- CreateTable
+CREATE TABLE "Continent" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+
+    CONSTRAINT "Continent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Currency" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "name_plural" TEXT NOT NULL,
+    "symbol" TEXT NOT NULL,
+    "symbol_native" TEXT NOT NULL,
+    "decimal_digits" INTEGER NOT NULL,
+    "rounding" INTEGER NOT NULL,
+
+    CONSTRAINT "Currency_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Country" (
+    "id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "alpha2Code" TEXT NOT NULL,
+    "alpha3code" TEXT NOT NULL,
+    "numberCode" INTEGER NOT NULL,
+    "continentCode" TEXT NOT NULL,
+    "capital" TEXT NOT NULL,
+    "phoneNumberCode" TEXT NOT NULL,
+    "currencyCode" TEXT NOT NULL,
+    "timezone" TEXT[],
+
+    CONSTRAINT "Country_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Flag" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "countryId" INTEGER NOT NULL,
+    "emoji" TEXT NOT NULL,
+    "unicode" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Flag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "State" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "countryId" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "State_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Continent_code_key" ON "Continent"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Currency_code_key" ON "Currency"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Flag_countryId_key" ON "Flag"("countryId");
+
+-- AddForeignKey
+ALTER TABLE "Country" ADD CONSTRAINT "Country_continentCode_fkey" FOREIGN KEY ("continentCode") REFERENCES "Continent"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Flag" ADD CONSTRAINT "Flag_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "State" ADD CONSTRAINT "State_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
